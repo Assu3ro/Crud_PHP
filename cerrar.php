4ab25652c85a017121967a65ae764ae886563245
Emanuel Assuero <63565141+Assu3ro@users.noreply.github.com>
2023-04-10 23:56:43 -0300

    S/Pasta

---
+++ cerrar.php
@@ -0,0 +1,6 @@
+<?php include("../templates/header.php"); ?>
+
+<div>Clique aqui para sair</div>
+
+
+<?php include("../templates/footer.php"); ?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include("templates/header.php"); ?>
+
+</br>
+
+ <div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">Sejam Bem Vindos</h1>
+      <p class="col-md-8 fs-4">Nós somos a Fanal-Tec, maior empresa de Organização e Solucion Digital Problems, das Americas. <br>Sejam muito bem vindos!! </p>
+      <button class="btn btn-primary btn-lg" type="button">Saiba Mais!</button>
+    </div>
+   </div>
+
+
+   <?php include("templates/footer.php"); ?>
